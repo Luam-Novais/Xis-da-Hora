35080b29456ac5a38cbbb6c41d2b80a131c19b86
Luam-Novais <luampabl014@gmail.com>
2025-03-31 20:20:24 -0300

    feat(routes): add AppRoutes for application routing

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Auth from './pages/Auth';
+import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
+import GlobalContext from './context/GlobalContext';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-        <Header/>
+      <GlobalContext>
         <Routes>
+
+          //rotas login
+          
+          <Route path="/auth" element={<Auth />}>
+              <Route path="login" element={<Login />} />
+              <Route path="registrar" element={<CreateAccount />} />
+          </Route>
           <Route path="*" />
         </Routes>
+      </GlobalContext>
     </BrowserRouter>
   );
 };
