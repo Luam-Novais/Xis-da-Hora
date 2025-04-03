a790de0d97d6b109a9eb812f426352bed1b885b8
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:33:59 -0300

    feat: add new routes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
 import Auth from './pages/Auth';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
 import GlobalContext from './context/GlobalContext';
+import Home from './pages/Home';
+import Cardapio from './pages/Cardapio';
+import Carrinho from './pages/Carrinho';
+import MyAccount from './pages/MyAccount';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <GlobalContext>
+        <Header/>
         <Routes>
-
+          <Route path='/' element={<Home/>}/>
+          <Route path='cardapio' element={<Cardapio/>}/>
+          <Route path='carrinho' element={<Carrinho/>}/>
+          <Route path='minhaConta' element={<MyAccount/>}/>
           //rotas login
           
           <Route path="/auth" element={<Auth />}>
