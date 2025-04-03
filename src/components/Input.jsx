3a968ae68f6378958767fa898bea35a220a1c61e
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:13:16 -0300

    refactor(Input): update component structure and styles

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,11 +4,10 @@ import styles from '../styles/components/Input.module.scss'
 
 const Input = ({label, type, name, id, className, onBlur, ...props}) => {
      
-
   return (
        <div className={`${styles.containerInput} ${className}`}>
             <label htmlFor={id}>{label}</label>
-            <input 
+            <input
                type={type} 
                name={name} 
                id={id}
