86a1610ee94c3d6b39c82ad7ae9ba15100ca35f7
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:26:53 -0300

    feat(home): create Home page and add styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../styles/pages/Home.module.scss'
+import Button from '../components/Button'
+import RegisterBurger from './admin/RegisterBurger'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <section className={styles.hero} >
+        <div>
+            <h1>
+                PORQUE A FOME <br/>
+                NÃO TEM HORA!
+            </h1>
+            <Link to='cardapio'><Button>Fazer Pedido</Button></Link>
+        </div>
+        <img src="/burger-home.png" alt="" />
+    </section>
+  )
+}
+
+export default Home
