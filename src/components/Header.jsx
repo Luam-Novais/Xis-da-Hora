e1de29b00ea94e08d4c153846fec212b25e79d9d
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:10:45 -0300

    feat(header): add routing to links and improve mobile styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import styles from '../styles/components/Header.module.scss';
 import { NavLink } from 'react-router-dom';
+import { IoFastFood, IoBagHandle, IoPerson} from "react-icons/io5";
+import useMedia from '../hooks/useMedia';
 
 const Header = () => {
+  const mobile = useMedia('(max-width: 768px)')
+
   return (
-    <header className={styles.header}>
-      <img src="/logo.svg" alt="" />
-      <nav>
+    <header className={!mobile ? styles.header : styles.headerMobile}>
+      <NavLink to='/'><img className={styles.logo} src="/logo.svg" alt="" /></NavLink>
+      <nav className={styles.navContainer}>
         <ul>
           <li>
-            <NavLink>Cardapio</NavLink>
+            <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='cardapio'>
+              {!mobile ? 'Cardapio' :<i><IoFastFood/></i> }
+            </NavLink>
           </li>
           <li>
-            <NavLink>Carrinho</NavLink>
+            <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''}  to='carrinho'>
+              {!mobile ? 'Carrinho' :<i><IoBagHandle/></i> }
+            </NavLink>
           </li>
           <li>
-            <NavLink>Minha Conta</NavLink>
+            <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''}  to='minhaConta'>
+              {!mobile ? 'Minha Conta' :<i><IoPerson/></i> }
+            </NavLink>
           </li>
         </ul>
       </nav>
