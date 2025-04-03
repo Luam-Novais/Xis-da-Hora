7766a33ac9e6e2459c4e28914fff5cbf484c34ba
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:35:53 -0300

    feat: add context to users

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,73 +1,63 @@
-import React, { createContext, useEffect, useState} from 'react'
-import { TOKEN_POST, USER_GET } from '../utilities/api'
-import { useNavigate } from 'react-router-dom'
-export const userContext = createContext()
+import React, { createContext, useState } from 'react';
+import { DATA_POST } from '../utilities/api';
+import { useNavigate } from 'react-router-dom';
 
-export const UserStorage = ({children}) => {
+export const userContext = createContext();
+const UserStorage = ({ children }) => {
+  const [user, setUser] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-  const [user, setUser] = useState(null)
-  const [loading, setLoading] = useState(null)
+  async function userLogin(data) {
+    const { url, options } = DATA_POST('/auth/login', data);
+    setLoading(true);
+    try {
+      const response = await fetch(url, options);
+      const json = await response.json();
 
-  const userLogin = async (formData)=>{
-    const {url, options} = TOKEN_POST('/auth/login', formData)
-    setLoading(true)
-    try{
-        const response = await fetch(url, options)
-        const data = await response.json()
-        
-        if(response.ok){
-          window.localStorage.setItem('token', data.token)
-          setUser(data.user.nome)
-          navigate('/')
-        }
-        else {
-          throw new Error(data.message);
-        }
-     }
-     catch(err){
-        alert('Ocorreu um erro! ' + err)
-     }
-     finally{
-        setLoading(false)
-     }
-  }
-  const userCreate = async (formData)=>{
-    const {url, options} = TOKEN_POST('/auth/registrar',formData)
-    try{
-      const response = await fetch(url, options)
-      const data = await response.json()
-    
-      if(response.ok){
-          window.localStorage.setItem('token', data.token)
-          setUser(data.user.nome)
-          navigate('/')
+      if (response.ok) {
+        window.localStorage.setItem('token', json.token)
+        setUser(json.user.nome)
+        navigate('/');
       }
-    }catch(err){
-      console.log(err);
+    } catch (err) {
+      alert('Ocorreu um erro!', err);
+    } finally {
+      setLoading(false);
     }
-    
   }
-  // const verificarToken = async (token)=>{
-  //     const {url, options} = USER_GET('/auth/me', token)
-  //     const response = await fetch(url, options)
-  //     const data = await response.json()
+  async function userCreate(data) {
+    const { url, options } = DATA_POST('/auth/registrar', data);
+    setLoading(true);
+    try {
+      const response = await fetch(url, options);
+      const json = await response.json();
 
-  //     setUser(data.nome)
-  //     console.log(data)
-  // }
-  // useEffect(()=>{
-  //   const token = window.localStorage.getItem('token')
-  //   if(token){
-  //     verificarToken(token)
-  //   }else{
-  //     console.log('nada com nada')
-  //   }
-  // },[])
+      if (response.ok) {
+        window.localStorage.setItem('token', json.token)
+        setUser(json.user.nome)
+        navigate('/');
+        navigate('/');
+      }
+    } catch (err) {
+      alert('Ocorreu um erro!', err);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-      <userContext.Provider value={{user, setUser, userLogin, userCreate, loading}}>
-        {children}
-      </userContext.Provider>
-  )
-}
+    <userContext.Provider
+      value={{
+        user,
+        setUser,
+        userLogin,
+        userCreate,
+        loading,
+      }}
+    >
+      {children}
+    </userContext.Provider>
+  );
+};
 
+export default UserStorage;
