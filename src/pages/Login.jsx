28993c48b5a10a6d24e5d3156711b846f9b8c35a
Luam-Novais <luampabl014@gmail.com>
2025-04-02 23:28:10 -0300

    feat(Login): add Loading component and update styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,18 @@
 import React, { useContext} from 'react';
+import styles from '../styles/pages/Login.module.scss'
 import { Link } from 'react-router-dom';
-import FormContainer from '../components/FormContainer';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import useForm from '../hooks/useForm';
 import { userContext } from '../context/UserContext';
+import Loading from '../components/Loading'
 import Title from '../components/Title';
 
 const Login = () => {
-  const { userLogin} = useContext(userContext)
+  const { userLogin, loading } = useContext(userContext)
   const email = useForm('email');
   const senha = useForm('senha');
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = {
@@ -20,19 +21,21 @@ const Login = () => {
     };
     userLogin(formData)
   };
+  if(loading){
+    return <Loading/>
+  }
   return (
-    <>
-      <FormContainer onSubmit={handleSubmit}>
-        <img src="/logo.svg" alt="" className="logo full-width" />
-        <Title>Entrar</Title>
-        <Input label="Email" id="email" name="email" type="email" className="full-width" {...email} />
-        <Input label="Senha" id="senha" name="senha" type="password" className="full-width" {...senha} />
-        <Button className="full-width">Entrar</Button>
-        <p className="full-width">Ainda não tem uma conta ? crie agora!</p>
-        <Link to="/auth/registrar">Criar Conta</Link>
-      </FormContainer>
-      <img className="bgForm" src="/bg-form.jpg" alt="" />
-    </>
+    <section>
+      <form className={styles.form} onSubmit={handleSubmit}>
+          <img className={`${styles.logo} full-width`}src="/logo.svg" alt=""/>
+          <Title>Entrar</Title>
+          <Input label="Email" id="email" name="email" type="email" className="full-width" {...email} />
+          <Input label="Senha" id="senha" name="senha" type="password" className="full-width" {...senha} />
+          <Button className="full-width">Entrar</Button>
+          <p className="full-width">Ainda não tem uma conta ? crie agora!</p>
+          <Link to="/auth/registrar">Criar Conta</Link>
+      </form>
+    </section>
   );
 };
 
