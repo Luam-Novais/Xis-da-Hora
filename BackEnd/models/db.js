93e822e9e969995c79a2584de6404a44e90c9d7e
Yan Ramos Almeida <yanramos780@gmail.com>
2025-04-09 21:37:28 -0300

    V1.0 - Primeiro commit

--- BackEnd/models/db.js
+++ BackEnd/models/db.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize("xisdahora", "root", "root", {
+const sequelize = new Sequelize("railway", "root", "IqWecYwmfvOAEIEXmWCHAjwKoRbchtoy", {
 
-    host: "localhost",
+    host: "shuttle.proxy.rlwy.net",
+    port: 26222,
     dialect: "mysql"
 })
 
-// sequelize.authenticate()
-//     .then(() => console.log("Conectado ao banco de dados"))
-//     .catch((error) => console.log("Erro ao conectar ao banco de dados " + error))
+sequelize.authenticate()
+    .then(() => console.log("Conectado ao banco de dados"))
+    .catch((error) => console.log("Erro ao conectar ao banco de dados " + error))
 
 module.exports = { Sequelize, sequelize }
