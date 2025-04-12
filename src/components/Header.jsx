bed0c4717ea8cf4f8c6157f4abed8c98c4c91d59
Luam-Novais <luampabl014@gmail.com>
2025-04-11 21:38:26 -0300

    fix: add logic to verify name user and add in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from '../styles/components/Header.module.scss';
 import { NavLink } from 'react-router-dom';
-import { IoHome, IoFastFood, IoBagHandle, IoPerson} from "react-icons/io5";
+import { IoHome, IoFastFood, IoBagHandle, IoPerson, IoLogIn} from "react-icons/io5";
 import useMedia from '../hooks/useMedia';
+import { userContext } from '../context/UserContext';
 
 const Header = () => {
   const mobile = useMedia('(max-width: 768px)')
+  const {isAuthorized, user} = useContext(userContext)
+
+  const firstName = user.split(' ')[0]
+
+  console.log(firstName)
 
   return (
     <header className={!mobile ? styles.header : styles.headerMobile}>
       {!mobile && <NavLink to='/' ><img className={styles.logo} src="/logo.svg" alt="logo do xis da hora" /></NavLink>}
       <nav className={styles.navContainer}>
         <ul>
-          {mobile && <li> <NavLink to='/'><i><IoHome/></i></NavLink></li>}
+          {mobile && <li> 
+            <NavLink to='/' className={({isActive}) => isActive ? `${styles.active}` : ''}>
+            <i><IoHome/></i></NavLink></li>}
           <li>
             <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='cardapio'>
-              {!mobile ? 'Cardapio' :<i><IoFastFood/></i> }
+              {!mobile ? 'Cardapio' : <i><IoFastFood/></i> }
             </NavLink>
           </li>
           <li>
             <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''}  to='carrinho'>
-              {!mobile ? 'Carrinho' :<i><IoBagHandle/></i> }
+              {!mobile ? 'Carrinho' : <i><IoBagHandle/></i> }
             </NavLink>
           </li>
           <li>
             <NavLink className={({isActive}) => isActive ? `${styles.active}` : ''}  to='minhaConta'>
-              {!mobile ? 'Minha Conta' :<i><IoPerson/></i> }
+              {!mobile ? 
+              isAuthorized ? firstName :  <>Entrar <IoLogIn/></>
+               : 
+              <i>
+                {isAuthorized ? <IoPerson/> : <IoLogIn/>}
+              </i> }
             </NavLink>
           </li>
         </ul>
