a32f316a4b624a0d0337f6d52006efab4b420d86
Luam-Novais <luampabl014@gmail.com>
2025-04-11 21:32:25 -0300

    feat: add component footer and yours styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../styles/components/Footer.module.scss'
+import { IoLogoWhatsapp, IoLogoInstagram, IoLogoFacebook} from "react-icons/io";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a href="#"><img src="/logo.svg" alt="" /></a>
+        <p> Todos os direitos reservados&copy;. 2025 Xis da Hora.</p>
+        <div>
+          <h2>Redes Sociais</h2>
+          <ul>
+            <li><a href=""><IoLogoWhatsapp/></a></li>
+            <li><a href=""><IoLogoInstagram/></a></li>
+            <li><a href=""></a><IoLogoFacebook/></li>
+          </ul>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
