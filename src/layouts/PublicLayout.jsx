844e2b0888cd87249f7e5568aaba3c2269e1632c
Luam-Novais <luampabl014@gmail.com>
2025-04-11 21:44:14 -0300

    feat: add PublicLayout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import styles from '../styles/layouts/PublicLayout.module.scss'
 import { Outlet } from 'react-router-dom'
 
 const PublicLayout = () => {
   return (
     <>
         <Header/>
-        <Outlet/>
+        <main className={styles.container}>
+            <Outlet/>
+        </main>
         <Footer/>
     </>
   )
