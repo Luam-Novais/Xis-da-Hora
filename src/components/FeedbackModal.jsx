42b263303e5bf1e06dad36de17de93d442c5d0aa
Luam-Novais <luampabl014@gmail.com>
2025-04-14 22:14:03 -0300

    feat: add modal component to show request status

---
+++ src/components/FeedbackModal.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import styles from '../styles/components/FeedbackModal.module.scss';
+import { userContext } from '../context/UserContext';
+const FeedBackModal = () => {
+  const { errorModal, setErrorModal, message } = useContext(userContext);
+  if (errorModal) {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+  function handleClick() {
+    setErrorModal(false);
+  }
+
+  return (
+    <>
+      {errorModal && (
+        <div className={styles.overlay}>
+          <div className={styles.containerModal}>
+            <p>{message}</p>
+            <button onClick={handleClick}>ok</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FeedBackModal;
