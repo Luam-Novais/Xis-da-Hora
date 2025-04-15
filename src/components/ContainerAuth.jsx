6faa2ab1f9eb2acebdd6449c7109a4aa85c8121b
Luam-Novais <luampabl014@gmail.com>
2025-04-14 22:19:26 -0300

    feat: add component modal

--- src/components/ContainerAuth.jsx
+++ src/components/ContainerAuth.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from '../styles/components/ContainerAuth.module.scss'
+import FeedBackModal from './FeedBackModal'
 
 const ContainerAuth = ({children}) => {
   return (
     <section className={styles.containerAuth}>
+      <FeedBackModal/>
       {children}
     </section>
   )
