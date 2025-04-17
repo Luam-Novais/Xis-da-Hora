5392e9a38d3bc2abf1079efed6cda46dc7fbe080
Luam-Novais <luampabl014@gmail.com>
2025-04-17 18:01:13 -0300

    feat: add component Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,47 @@
+import React, { useReducer } from 'react'
+import {IoBagHandle} from "react-icons/io5";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+import styles from '../styles/components/Card.module.scss'
+
+const changeQuantity = (state, action) =>{
+    switch(action){
+        case 'increment' :{
+            return state + 1
+        }
+        case 'decrement':{
+            if(state > 0){
+                return state - 1
+            }else{
+                return state = 0
+            }
+        }
+        default :{
+            return console.log('action invalida')
+        }
+    }
+}
+const Card = () => {
+    const [quantity, dispatch] = useReducer(changeQuantity, 0)
+  return (
+    <div className={styles.card}>
+        <img src="/tropeiro.jpg" alt="" />
+        <div className={styles.content}>
+            <h2>X-tudão</h2>
+            <p>Pão, carne, queijo, alface e tomate</p>
+            <p><b>R$35,90</b></p>
+
+            <div className={styles.buttonsContainer}>
+                <span className={styles.quantityContainer}>
+                    <button onClick={()=> dispatch('decrement')}><FaMinus/></button>
+                    <p>{quantity}</p>
+                    <button onClick={()=> dispatch('increment')}><FaPlus/></button>
+                </span>
+                <button className={styles.addButton}>adicionar <i><IoBagHandle/></i></button>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default React.memo(Card)
