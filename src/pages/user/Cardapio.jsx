d411958f76297f5a67d36ccbdf1ad503538bbe16
Luam-Novais <luampabl014@gmail.com>
2025-04-17 18:07:08 -0300

    feat: Cardapio page (in phase testing)

--- src/pages/user/Cardapio.jsx
+++ src/pages/user/Cardapio.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from '../../styles/pages/user/Cardapio.module.scss'
+import { NavLink, Outlet, useParams } from 'react-router-dom'
+import MenuContent from '../../components/MenuContent'
 
 const Cardapio = () => {
+  const {id} = useParams()
   return (
-    <div>Cardapio</div>
+    <section className={styles.container}>
+      <nav className={styles.navbar}>
+        <ul>
+          <li><NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='/cardapio/burgers'>hamburguer</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='/cardapio/porcoes'>porções</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='/cardapio/bebidas'>bebidas</NavLink></li>
+          <li><NavLink className={({isActive}) => isActive ? `${styles.active}` : ''} to='/cardapio/sobremesas'>sobremesas</NavLink></li>
+        </ul>
+      </nav> 
+      <MenuContent/>
+    </section>
   )
 }
 
