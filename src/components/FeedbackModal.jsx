c570973e996c79a57fbab79fb1bb52c2dd203e93
Luam-Novais <luampabl014@gmail.com>
2025-04-21 18:46:37 -0300

    feat: add status modal funcionality

--- src/components/FeedbackModal.jsx
+++ src/components/FeedbackModal.jsx
@@ -1,25 +1,31 @@
 import React, { useContext } from 'react';
 import styles from '../styles/components/FeedbackModal.module.scss';
+import '../styles/_variables.scss'
 import { userContext } from '../context/UserContext';
-const FeedBackModal = () => {
-  const { errorModal, setErrorModal, message } = useContext(userContext);
-  if (errorModal) {
+import { useNavigate } from 'react-router-dom';
+const FeedBackModal = ({status}) => {
+  const { statusModal, setStatusModal, message } = useContext(userContext);
+  const navigate = useNavigate()
+  if (statusModal !== null) {
     window.scrollTo({ top: 0, behavior: 'instant' });
     document.body.style.overflow = 'hidden';
   } else {
     document.body.style.overflow = '';
   }
   function handleClick() {
-    setErrorModal(false);
+    setStatusModal(null);
+    if(statusModal){
+      navigate('/')
+    }
   }
 
   return (
     <>
-      {errorModal && (
+      {statusModal !==null && (
         <div className={styles.overlay}>
-          <div className={styles.containerModal}>
-            <p>{message}</p>
-            <button onClick={handleClick}>ok</button>
+          <div style={{borderColor: status ? 'green' : 'red' }} className={styles.containerModal}>
+            <p>{statusModal ? 'Usuário cadastrado com sucesso!' : message}</p>
+            <button style={{backgroundColor: status ? 'green' : 'red'}} onClick={handleClick}>ok</button>
           </div>
         </div>
       )}
