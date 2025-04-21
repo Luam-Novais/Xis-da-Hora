0da53f5a8131e14f4386d546bc99ca37153aba9e
Luam-Novais <luampabl014@gmail.com>
2025-04-21 18:44:11 -0300

    fix: add status modal funcionality

--- src/components/ContainerAuth.jsx
+++ src/components/ContainerAuth.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from '../styles/components/ContainerAuth.module.scss';
 import FeedbackModal from '../components/FeedbackModal';
+import { userContext } from '../context/UserContext';
 
 const ContainerAuth = ({ children }) => {
+  const {statusModal} = useContext(userContext)
   return (
     <section className={styles.containerAuth}>
-      <FeedbackModal />
+      <FeedbackModal  status={statusModal}/>
       {children}
     </section>
   );
