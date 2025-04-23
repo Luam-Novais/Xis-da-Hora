521325e5299f69051903f8f0905af3c9dc1347b4
Luam-Novais <luampabl014@gmail.com>
2025-04-23 16:23:22 -0300

    feat: add component Password, to controller input value is visible or no

---
+++ src/components/PasswordInput.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import styles from '../styles/components/PasswordInput.module.scss';
+import { LuEye, LuEyeClosed } from 'react-icons/lu';
+
+const PasswordInput = ({ label, name, id, className, onBlur, ...props }) => {
+  const [showPass, setShowPass] = useState(false);
+
+  function handleShowPass() {
+    setShowPass(!showPass);
+  }
+  return (
+    <div className={`${styles.containerInput} ${className}`}>
+      <label htmlFor={id}>{label}</label>
+      <span>
+        <input type={showPass ? 'text' : 'password'} name={name} id={id} value={props.value} onChange={props.onChange} onBlur={onBlur} />
+        <button type="button" onClick={handleShowPass}>
+          <i>{showPass ? <LuEye /> : <LuEyeClosed />}</i>
+        </button>
+      </span>
+      <p className={styles.error}>{props.error && props.error}</p>
+    </div>
+  );
+};
+
+export default PasswordInput;
