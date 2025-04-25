f61b913eeee0f6ee221077937f07025ef4a6f979
Luam-Novais <luampabl014@gmail.com>
2025-04-24 21:31:03 -0300

    feat: enhance MenuContent component with data fetching and loading state

--- src/components/layout/MenuContent.jsx
+++ src/components/layout/MenuContent.jsx
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import styles from '../../styles/components/layout/MenuContent.module.scss'
 import  Card from '../common/Card'
+import Loading from '../common/Loading'
+import { useParams } from 'react-router-dom'
+import { urlProd } from '../../utilities/urls'
+import useFetch from '../../hooks/useFetch'
 
 const MenuContent = () => {
-
+  const {id} = useParams()
+  const {data, request, loading, error} = useFetch()
+  useEffect(()=>{
+      request(urlProd + 'cardapio/' + id)
+  },[id])
+  if(loading){
+    return <Loading/>
+  }
+  if(data){
     return (
       <div className={styles.container}>
-        <Card/>
+        {data.map((hamburger) =>{
+          return (
+            <Card key={hamburger.id} src={hamburger.imagem} nome={hamburger.nome} ingredientes={hamburger.ingredientes}
+            valor={hamburger.valor}/>
+          )
+        })}
       </div>
     )
+  }
+   
 }
 
 export default MenuContent
