008e67d82063bf6b6aa6f2972177c171e6929dd2
Luam-Novais <luampabl014@gmail.com>
2025-04-24 21:22:46 -0300

    feat: add new custom hook for requests

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useState } from 'react'
+
+const useFetch = () => {
+      const [data, setData] = useState(null)
+      const [loading, setLoading] = useState(false)
+      const [error, setError] = useState(null)
+
+    const request = useCallback(async (url)=>{
+        let response;
+        let json;
+
+        try{
+            setLoading(true)
+            response = await fetch(url)
+            json = await response.json()
+
+            if(!response.ok) throw new Error(json.message)
+        }catch(err){
+            setError(err)
+            alert(err + 'Ocorreu um erro ao carregar os recursos. Verifique sua conexão.')
+        }
+        finally{
+            setLoading(false)
+            setData(json)
+            return{
+                json, response
+            }
+        }
+    }, [])
+
+  return {
+    request,
+    data,
+    loading, 
+    error
+  }
+}
+
+export default useFetch
