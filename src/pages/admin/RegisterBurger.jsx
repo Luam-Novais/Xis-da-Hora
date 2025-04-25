0e8a54bb2be982435cf3d82e458afe1e98779fe2
Luam-Novais <luampabl014@gmail.com>
2025-04-24 21:27:55 -0300

    fix: improving the food saving function

--- src/pages/admin/RegisterBurger.jsx
+++ src/pages/admin/RegisterBurger.jsx
@@ -1,86 +1,120 @@
-import React, { useState } from 'react'
-import { IoMdCloudUpload } from "react-icons/io";
-import Title from '../../components/Title'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
-import styles from '../../styles/pages/admin/RegisterBurger.module.scss'
-import useForm from '../../hooks/useForm'
+import React, { useState } from 'react';
+import { IoMdCloudUpload } from 'react-icons/io';
+import Title from '../../components/common/Title';
+import Loading from '../../components/common/Loading';
+import Input from '../../components/common/Input';
+import Button from '../../components/common/Button';
+import styles from '../../styles/pages/admin/RegisterBurger.module.scss';
+import useForm from '../../hooks/useForm';
 
 const RegisterBurger = () => {
+    const [loading, setLoading] = useState(false)
     const nome = useForm('nome')
     const valor = useForm('valor')
-    const [ingredients, setIngredients] = useState('') 
+    const [categoria, setCategoria] = useState('')
     const [media, setMedia] = useState(null)
+    const [ingredientes, setIngredientes] = useState('')
     const [previewImg, setPreviewImg] = useState(null)
 
-    function handleIngredients({target}){
-        setIngredients(target.value)
+    function handleIngredientes({target}){
+        setIngredientes(target.value)
+    }
+    function handleSelect({target}){
+        setCategoria(target.value)
     }
 
     function handleMedia({target}){
-       const file = target.files[0]
-       if(file){
-            setPreviewImg(URL.createObjectURL(file))
-            setMedia(file)
-       }else{
-            setPreviewImg(null)
-       }
+        const file = URL.createObjectURL(target.files[0])
+        if(file){
+            setPreviewImg(file)
+        }
+        setMedia(target.files[0])
     }
-    
+
     async function handleSubmit(event){
         event.preventDefault()
         const formData = new FormData()
         if(
-            media && 
-            nome.value && 
-            ingredients &&
-            valor.value
+            media &&
+            nome.value &&
+            ingredientes && 
+            categoria &&
+            valor.value 
         ){
             formData.append('imagem', media)
             formData.append('nome', nome.value)
-            formData.append('ingredientes', ingredients)
+            formData.append('categoria', categoria)
             formData.append('valor', valor.value)
-        }
-        
-        console.log([...formData.entries()])
-        const response = await fetch('http://26.252.150.176:3001/hamburguers', {
-            method: 'POST',
-            body: formData
-        })
+            formData.append('ingredientes', ingredientes)
 
-        console.log(response)
-    }
-    return (
-        <section className={styles.container}>
-            <Title>Registrar Hamburger</Title>
-            <form action="" onSubmit={handleSubmit}>
-                    <label htmlFor="imagem" className={styles.fileUpload}>
-                        Selecionar Imagem <IoMdCloudUpload />
-                        <input type='file' id='imagem' name='imagem' onChange={handleMedia}/>
-                    </label>
-                {
-                    previewImg ? 
-                    <picture className={styles.containerImg}>
-                        {previewImg && 
-                            <span>
-                                <img src={previewImg} alt="" />
-                            </span>}
-                    </picture> : 
-                    <p>Selecione um arquivo.</p>
+            try{
+                setLoading(true)
+                const response = await fetch('https://xisdahora.up.railway.app/hamburguers', {
+                    method: 'POST',
+                    body: formData
+                })
+                const json = await response.json()
+                console.log(response, json)
+                if(response.ok){
+                    alert('Alimento cadastrado com sucesso!')
+                    setMedia(null)
+                    setIngredientes('')
+                    setPreviewImg(null)
+                    setCategoria(null)
+                    nome.value === ''
+                    valor.value === ''
                 }
-                <Input type='text' name='nome' id='nome' label='Nome do alimento' {...nome}/>
-                <span>
-                    <label htmlFor="ingredinetes"> Ingredientes / Descrição</label>
-                    <textarea id='ingredinetes' name='ingredinetes' value={ingredients} onChange={handleIngredients}/>
-                </span>
-                <Input type='text' name='valor' id='valor' label='Valor' {...valor}/>
+            }catch(err){
+                alert('Ocorreu um erro inesperado. Por favor verifique sua conexão!')
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        else{
+           alert('Por favor, preencha todos os dados.')
+        }
+    }
 
-                <Button>Registrar</Button>
+    if(loading){
+         return <Loading/>
+    }
+  return (
+    <section className={styles.container}>
+      <form action="" onSubmit={handleSubmit}>
+      <Title>Registrar Hamburger</Title>
+        <label htmlFor="imagem" className={styles.fileUpload}>
+          Selecionar Imagem <IoMdCloudUpload />
+          <input type="file" id="imagem" name="imagem"  onChange={handleMedia}/>
+        </label>
+        {previewImg ? (
+          <picture className={styles.containerImg}>
+            {previewImg && (
+              <span>
+                <img src={previewImg} alt="" />
+              </span>
+            )}
+          </picture>
+        ) : (
+          <p>Nenhum arquivo selecionado.</p>
+        )}
+        <Input type="text" name="nome" id="nome" label="Nome do alimento" {...nome} />
+        <span>
+          <label htmlFor="ingredinetes"> Ingredientes / Descrição</label>
+          <textarea id="ingredientes" name="ingredinetes" value={ingredientes} onChange={handleIngredientes} />
+        </span>
+        <select name="categoria" id="categoria" onChange={handleSelect}>
+            <option value="">Selecione uma categoria</option>
+            <option value="hamburguer">Hamburguer</option>
+            <option value="porcoes">Porções</option>
+            <option value="bebidas">Bebidas</option>
+            <option value="sobremesas">Sobremesas</option>
+        </select>
+        <Input type="text" name="valor" id="valor" label="Valor" {...valor} />
+        <Button>Registrar</Button>
+      </form>
+    </section>
+  );
+};
 
-            </form>
-
-        </section>
-    )
-}
-
-export default RegisterBurger
+export default RegisterBurger;
