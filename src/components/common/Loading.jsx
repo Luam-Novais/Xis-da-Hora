85b7dff6adfbd9cbfec26877c8e0f70c8503901f
Luam-Novais <luampabl014@gmail.com>
2025-04-24 21:21:44 -0300

    fix: changing structure component

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import styles from '../../styles/components/common/Loading.module.scss'
+import styles from '../../styles/components/common/Loading.module.scss';
 const Loading = () => {
   return (
-    <svg className={styles.loading} viewBox="25 25 50 50">
-      <circle r="20" cy="50" cx="50"></circle>
-    </svg>
+    <div className={styles.container}>
+      <svg className={styles.loading} viewBox="25 25 50 50">
+        <circle r="20" cy="50" cx="50"></circle>
+      </svg>
+    </div>
   );
 };
 
