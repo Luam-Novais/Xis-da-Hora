98ce3190db70778a413357285fa0461d0dcf4d68
Luam-Novais <luampabl014@gmail.com>
2025-04-28 21:34:44 -0300

    feat: add jwt-decode dependency; remove RouteAuthorization component; clean up AdminLayout imports

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../components/layout/Header';
+import { Outlet } from 'react-router-dom'; 
 
 const AdminLayout = () => {
   return (
