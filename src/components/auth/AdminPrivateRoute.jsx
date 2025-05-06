692d09b77598739ef1792a64bd3af315a6467479
Luam-Novais <luampabl014@gmail.com>
2025-05-06 07:26:32 -0300

    fix: add login redirect for unauthenticated users in AdminPrivateRoute

--- src/components/auth/AdminPrivateRoute.jsx
+++ src/components/auth/AdminPrivateRoute.jsx
@@ -4,10 +4,7 @@ import { Navigate } from 'react-router-dom'
 
 const AdminPrivateRoute = ({children}) => {
   const {user} = useContext(userContext)
-  
-  if(!user){
-    return <Navigate to='/auth/login'/>
-  }
+
   if(user && !user.admin){
     return <Navigate to='/unauthorized' replace/>
   }else{
