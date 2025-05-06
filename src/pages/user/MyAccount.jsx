d75c63bc98dbdd9a52dc54b39edf0aaa04f38f30
Luam-Novais <luampabl014@gmail.com>
2025-05-06 07:34:05 -0300

    feat: add admin link to MyAccount component

--- src/pages/user/MyAccount.jsx
+++ src/pages/user/MyAccount.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { userContext } from '../../context/UserContext'
 
 const MyAccount = () => {
   const {logout} = useContext(userContext)
   return (
     <div>MyAccount
+      <Link to='/admin/functions'>admin</Link>
       <button onClick={logout}>Logout</button>
     </div>
   )
