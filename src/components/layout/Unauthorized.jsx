3b755f3f405481b91599edced56346d165fb35f2
Luam-Novais <luampabl014@gmail.com>
2025-05-06 07:30:11 -0300

    feat: add Unauthorized component with user guidance and login link feat: implement MessageCart component for displaying cart status messages

--- src/components/layout/Unauthorized.jsx
+++ src/components/layout/Unauthorized.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { FaLock } from "react-icons/fa";
 
 const Unauthorized = () => {
   return (
-    <div>Usuario não autorizado!
+    <div>
+        <h3>Ops! Essa parte do sistema é exclusiva para quem gerencia o cardápio.</h3>
+        <p>Se você é administrador, verifique seu login ou entre em contato com o suporte. <i><FaLock/></i></p>
         <Link to='/auth/login'>Login</Link>
     </div>
   )
---
+++ src/components/modals/MessageCart.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from 'react';
+import { FaCheck } from 'react-icons/fa6';
+import { FaTimes } from 'react-icons/fa';
+import styles from '../../styles/components/modals/MessageCart.module.scss'
+import { cartContext } from '../../context/CartContext';
+
+const MessageCart = () => {
+  const { status } = useContext(cartContext);
+
+  useEffect(() => {
+    console.log(status);
+  }, [status]);
+  if (status.visible) {
+    return (
+      <div 
+      className={`${styles.container}`} 
+      style={{borderColor: status.state ? 'green': 'red'}}>
+        {status.message}
+        <i className={status.state ? styles.sucess : styles.error}>{status.state ? <FaCheck /> : <FaTimes />}</i>
+      </div>
+    );
+  }
+};
+
+export default MessageCart;
