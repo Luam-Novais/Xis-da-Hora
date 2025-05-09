3053adaf2b36874f454711fb2f5a2126fbe8e1dd
Luam-Novais <luampabl014@gmail.com>
2025-05-09 07:53:07 -0300

    fix: implement data fetching and loading state in Home component

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import styles from '../../styles/pages/user/Home.module.scss';
-import RegisterBurger from '../admin/RegisterBurger';
+import useFetch from '../../hooks/useFetch'
+import {urlProd} from '../../utilities/urls'
+import Loading from '../../components/common/Loading'
+
 
 const Home = () => {
-  
+  const {request, data, loading, error} = useFetch()
+
+  useEffect(()=>{
+    request(`${urlProd}cardapio/burgers`)
+  },[])
+
+  const news = useMemo(()=> data ? data.slice(-2) : [], [data])
+  console.log(news)
   return (
-
-
     <div className={styles.container}>
       <span className={styles.hero}></span>
       <ul className={styles.content}>
-        <h1>Novidades<span>.</span></h1>
+        <h1>
+          Novidades<span>.</span>
+        </h1>
+        {
+          loading ? <Loading/> :
+          news.map((item) =>{
+            return(
+              <li key={item.id}>
+                <img src={`${urlProd}uploads/${item.imagem}`} alt="" />
+                <div>
+                  <h2>{item.nome}</h2>
+                <p>{item.ingredientes}</p>
+                <p className={styles.valor}><b>R$ {item.valor}</b></p>
+                </div>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   );
