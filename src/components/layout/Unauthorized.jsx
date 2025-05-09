dd55eb55d3062aa110db8d86c801087aa32620e7
Luam-Novais <luampabl014@gmail.com>
2025-05-08 22:07:02 -0300

    feat: implement Unauthorized component with styled container and updated layout

--- src/components/layout/Unauthorized.jsx
+++ src/components/layout/Unauthorized.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { FaLock } from "react-icons/fa";
+import styles from '../../styles/components/layout/Unauthorized.module.scss'
 
 const Unauthorized = () => {
   return (
-    <div>
-        <h3>Ops! Essa parte do sistema é exclusiva para quem gerencia o cardápio.</h3>
-        <p>Se você é administrador, verifique seu login ou entre em contato com o suporte. <i><FaLock/></i></p>
-        <Link to='/auth/login'>Login</Link>
+    <div className={styles.container}>
+        <h1>Ops! Essa parte do sistema é exclusiva para quem gerencia o cardápio.</h1>
+        <p>Se você é administrador, verifique seu login ou entre em contato com o suporte.<i><FaLock/></i></p>
+        <Link to='/minhaConta'>Login</Link>
     </div>
   )
 }
