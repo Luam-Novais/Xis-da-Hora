ae39ffc5ef6f46829159310eab4b358262f4b584
Luam-Novais <luampabl014@gmail.com>
2025-05-08 22:12:59 -0300

    fix: remove duplicate menu items from Home component for cleaner layout

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -3,6 +3,7 @@ import styles from '../../styles/pages/user/Home.module.scss';
 import RegisterBurger from '../admin/RegisterBurger';
 
 const Home = () => {
+  
   return (
 
 
@@ -10,26 +11,6 @@ const Home = () => {
       <span className={styles.hero}></span>
       <ul className={styles.content}>
         <h1>Novidades<span>.</span></h1>
-        <li>
-          <img src="/tropeiro.jpg" alt="" />
-          <div>
-            <h2>X-tudão</h2>
-            <p>Pão, carne, queijo, alface e tomate</p>
-            <p>
-              <b>R$35,90</b>
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="/tropeiro.jpg" alt="" />
-          <div>
-            <h2>X-tudão</h2>
-            <p>Pão, carne, queijo, alface e tomate</p>
-            <p>
-              <b>R$35,90</b>
-            </p>
-          </div>
-        </li>
       </ul>
     </div>
   );
